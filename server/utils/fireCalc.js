4219bc3e7854212cffb89da3b49e87b24bb84b43
coderaniii <itzanjalianand@gmail.com>
2026-03-27 02:02:22 +0530

    backend added

--- server/utils/fireCalc.js
+++
@@ -1,9 +0,0 @@
-export function firePlan(data) {
-  const annualExpenses = data.expenses * 12;
-  const target = annualExpenses * 25;
-
-  const months = data.years * 12;
-  const sip = target / months;
-
-  return { target, sip };
-}
